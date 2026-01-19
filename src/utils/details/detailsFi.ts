00719dc407fefb16e5fb5bb4e10b7f17a6f82b2d
Teemu Orava <teemu.orava@kapsi.fi>
2026-01-19 16:10:44 +0200

    Separate details

---
+++ src/utils/details/detailsFi.ts
@@ -0,0 +1,117 @@
+export const detailsFi = {
+  weighting: {
+    weighed: ['punnittu'],
+    stone: ['kivineen'],
+    stoneless: ['kivetön'],
+    withpeel: ['kuorineen'],
+    peeled: ['kuorittu'],
+    average: ['average', 'tuotekeskiarvo', 'keskiarvo'],
+    kilogram: ['kg']
+  },
+  size: {
+    PORTS: ['small', 'pieni', 'pienet'],
+    PORTM: ['medium', 'keskikokoinen'],
+    PORTL: ['large', 'big', 'iso', 'isot']
+  },
+  cooking: {
+    boiled: ['boiled', 'keitetty'],
+    battered: ['battered', 'leivitetty'],
+    deepFried: ['deep fried', 'uppopaistettu'],
+    fried: ['paistettu'],
+    grilled: ['grillattu'],
+    fresh: ['fresh', 'tuore'],
+    dried: ['kuivattu'],
+    withegg: ['kananmunaa'],
+    thickened: ['suurustettu'],
+    nonthickened: ['suurustamaton'],
+    withoutsauce: ['ei kastiketta'],
+    coldsmoked: ['kylmäsavustettu', 'kylmäsavu'],
+    smoked: ['savustettu', 'savu'],
+    milk: ['kevytmaito', 'rasvaton maito'],
+    overcooked: ['ylikypsä', 'overcooked'],
+    cooked: ['cooked'],
+    grated: ['grated']
+  },
+  serving: {
+    servedWith: ['served with', 'kanssa'],
+    includes: ['includes', 'sisältää']
+  },
+  spicing: {
+    salted: ['suolattu', 'suolaa'],
+    withoutsalt: ['without salt', 'suolaton'],
+    withtomato: ['tomaattinen'],
+    withchocolate: ['suklainen'],
+    sugared: ['sokeroitu'],
+    nonsugared: ['sokeroimaton'],
+    flavored: ['maustettu'],
+    nonflavored: ['maustamaton', 'naturel']
+  },
+  description: {
+    fresh: ['fresh', 'raikas'],
+    soft: ['soft', 'pehmeä']
+  },
+  type: {
+    natural: ['luomu'],
+    bulk: ['irto'],
+    pott: ['ruukku'],
+    withfat: ['rasvaa'],
+    nonfat: ['rasvaton'],
+    lowfat: ['vähärasvainen'],
+    foam: ['vaahtoutuva'],
+    sliced: ['slice', 'sliced', 'paloiteltu', 'palat', 'pala', 'viipale', 'viipaloitu'],
+    nonlactose: ['laktoositon'],
+    thickened: ['puuroutuva'],
+    parboiled: ['kiehautettu', 'parboiled'],
+    lowlactose: ['vähälaktoosinen', 'hyla'],
+    uht: ['uht'],
+    insaltwater: ['suolavedessä', 'suolaved'],
+    frozenfood: ['pakasteateria', 'pakastettu', 'pakaste'],
+    bag: ['pussi'],
+    glutenfree: ['gluteeniton', 'gton'],
+    vitamin: ['d-vitaminoitu', 'vitaminoitu'],
+    canned: ['säilyke'],
+    fairtrade: ['reilun kaupan'],
+    vegan: ['vegaaninen', 'vegan'],
+    fresh: ['fresh', 'tuore'],
+    meal: ['meal', 'ateria'],
+    industrial: ['teollinen'],
+    
+    sweetorange: ['sweet orange'],
+
+    grannysmith: ['granny smith'],
+    golden: ['golden'],
+    royalgala: ['royal gala'],
+
+    tarocco: ['tarocco'],
+    moro: ['moro'],
+    sanguinello: ['sanguinello'],
+
+    nadorcott: ['nadorcott'],
+    bruno: ['bruno'],
+
+    yellow: ['keltainen'],
+    orange: ['orange'],
+    red: ['punainen'],
+    green: ['vihreä'],
+    white: ['valkoinen'],
+
+    skin: ['kuorellinen'],
+    withoutskin: ['kuoreton'],
+    package: ['paperipakkaus'],
+
+    filledPasta: ['täytepasta'],
+    filling: ['filling', 'täyte']
+  },
+  origin: {
+    local: ['kotimainen', 'kotimaiset'],
+    imported: ['ulkomainen', 'ulkomaiset'],
+    finnish: ['suomi', 'suomalainen', 'suomesta'],
+    californian: ['kalifornia', 'kalifornialainen'],
+    spanish: ['espanja', 'espanjalainen']
+  },
+  extra: {
+    trademark: ['™'],
+    registered: ["®"],
+    with: ['with a', 'with']
+  },
+};
--- src/utils/transactions.ts
+++ src/utils/transactions.ts
@@ -9,133 +9,18 @@ import { LevenshteinDistance } from "./levenshteinDistance";
 import { measureRegExp } from './receipts';
 import { Locale, NameTranslations } from "./types";
 import BrandShape from "../models/Brand";
+import { detailsFi } from "./details/detailsFi";
 
 export const getNumber = (value: string) => parseFloat(value.replace('−', '-').replace(',', '.'));
 
 export const getDetails = (brands: BrandShape[] = []) => {
-  const details: Record<string, Record<string, string[]>> = {};
+  const details: Record<string, Record<string, string[]>> = {...detailsFi};
 
   details.brands = {};
   
   brands.forEach(brand => {
     details.brands[brand.name] = [brand.name, ...brand.aliases || []];
   });
-
-  details.weighting = {
-    weighed: ['punnittu'],
-    stone: ['kivineen'],
-    stoneless: ['kivetön'],
-    withpeel: ['kuorineen'],
-    peeled: ['kuorittu'],
-    average: ['average', 'tuotekeskiarvo', 'keskiarvo'],
-    kilogram: ['kg']
-  };
-  details.size = {
-    PORTS: ['small', 'pieni', 'pienet'],
-    PORTM: ['medium', 'keskikokoinen'],
-    PORTL: ['large', 'big', 'iso', 'isot']
-  };
-  details.cooking = {
-    boiled: ['boiled', 'keitetty'],
-    battered: ['battered', 'leivitetty'],
-    deepFried: ['deep fried', 'uppopaistettu'],
-    fried: ['paistettu'],
-    grilled: ['grillattu'],
-    fresh: ['fresh', 'tuore'],
-    dried: ['kuivattu'],
-    withegg: ['kananmunaa'],
-    thickened: ['suurustettu'],
-    nonthickened: ['suurustamaton'],
-    withoutsauce: ['ei kastiketta'],
-    coldsmoked: ['kylmäsavustettu', 'kylmäsavu'],
-    smoked: ['savustettu', 'savu'],
-    milk: ['kevytmaito', 'rasvaton maito'],
-    overcooked: ['ylikypsä', 'overcooked'],
-    cooked: ['cooked'],
-    grated: ['grated']
-  };
-  details.serving = {
-    servedWith: ['served with', 'kanssa'],
-    includes: ['includes', 'sisältää']
-  };
-  details.spicing = {
-    salted: ['suolattu', 'suolaa'],
-    withoutsalt: ['without salt', 'suolaton'],
-    withtomato: ['tomaattinen'],
-    withchocolate: ['suklainen'],
-    sugared: ['sokeroitu'],
-    nonsugared: ['sokeroimaton'],
-    flavored: ['maustettu'],
-    nonflavored: ['maustamaton', 'naturel']
-  };
-  details.description = {
-    fresh: ['fresh', 'raikas'],
-    soft: ['soft', 'pehmeä']
-  };
-  details.type = {
-    natural: ['luomu'],
-    bulk: ['irto'],
-    pott: ['ruukku'],
-    withfat: ['rasvaa'],
-    nonfat: ['rasvaton'],
-    lowfat: ['vähärasvainen'],
-    foam: ['vaahtoutuva'],
-    sliced: ['slice', 'sliced', 'paloiteltu', 'palat', 'pala', 'viipale', 'viipaloitu'],
-    nonlactose: ['laktoositon'],
-    thickened: ['puuroutuva'],
-    parboiled: ['kiehautettu', 'parboiled'],
-    lowlactose: ['vähälaktoosinen', 'hyla'],
-    uht: ['uht'],
-    insaltwater: ['suolavedessä', 'suolaved'],
-    frozenfood: ['pakasteateria', 'pakastettu', 'pakaste'],
-    bag: ['pussi'],
-    glutenfree: ['gluteeniton', 'gton'],
-    vitamin: ['d-vitaminoitu', 'vitaminoitu'],
-    canned: ['säilyke'],
-    fairtrade: ['reilun kaupan'],
-    vegan: ['vegaaninen', 'vegan'],
-    fresh: ['fresh', 'tuore'],
-    meal: ['meal', 'ateria'],
-    industrial: ['teollinen'],
-    
-    sweetorange: ['sweet orange'],
-
-    grannysmith: ['granny smith'],
-    golden: ['golden'],
-    royalgala: ['royal gala'],
-
-    tarocco: ['tarocco'],
-    moro: ['moro'],
-    sanguinello: ['sanguinello'],
-
-    nadorcott: ['nadorcott'],
-    bruno: ['bruno'],
-
-    yellow: ['keltainen'],
-    orange: ['orange'],
-    red: ['punainen'],
-    green: ['vihreä'],
-    white: ['valkoinen'],
-
-    skin: ['kuorellinen'],
-    withoutskin: ['kuoreton'],
-    package: ['paperipakkaus'],
-
-    filledPasta: ['täytepasta'],
-    filling: ['filling', 'täyte']
-  };
-  details.origin = {
-    local: ['kotimainen', 'kotimaiset'],
-    imported: ['ulkomainen', 'ulkomaiset'],
-    finnish: ['suomi', 'suomalainen', 'suomesta'],
-    californian: ['kalifornia', 'kalifornialainen'],
-    spanish: ['espanja', 'espanjalainen']
-  };
-  details.extra = {
-    trademark: ['™'],
-    registered: ["®"],
-    with: ['with a', 'with']
-  };
 
   return details;
 };
